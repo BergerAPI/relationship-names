e1e545f5ac0e72696ed107b89e3f5ac8c39f26ad
niclas <bergerbtw@gmail.com>
2024-08-29 21:54:22 +0200

    feat: basic implementation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
+import 'dotenv/config'
+
 const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
+const FIRST = ALPHABET.indexOf("n")
+const SECOND = ALPHABET.indexOf("l")
+
+const fetchCollection = async (id: string, page: number = 0) => {
+	const req = await fetch(`https://api.unsplash.com/collections/${id}/photos?client_id=${process.env.UNSPLASH_ACCESS}&per_page=30&page=${page}`)
+	const json = await req.json()
+
+	return json as any[]
+}
+
 (async () => {
-	console.log("asdfsafd")
+	const collection = await fetchCollection("4760062", Math.floor((FIRST * SECOND) / 30))
+
+	const image = collection[Math.abs((FIRST * SECOND) % 30)]
+
+	console.log(image.urls.raw)
 })().then(_ => {
 	/* */
 })
