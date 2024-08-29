dec70e48c326a8575b1214eac09833ca7e92df04
BergerAPI <bergerbtw@gmail.com>
2024-08-29 22:09:16 +0200

    feat: express

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
+import express from "express"
 import 'dotenv/config'
 
 const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-
-const FIRST = ALPHABET.indexOf("n")
-const SECOND = ALPHABET.indexOf("l")
 
 const fetchCollection = async (id: string, page: number = 0) => {
 	const req = await fetch(`https://api.unsplash.com/collections/${id}/photos?client_id=${process.env.UNSPLASH_ACCESS}&per_page=30&page=${page}`)
@@ -13,11 +11,34 @@ const fetchCollection = async (id: string, page: number = 0) => {
 }
 
 (async () => {
-	const collection = await fetchCollection("4760062", Math.floor((FIRST * SECOND) / 30))
+	const app = express()
 
-	const image = collection[Math.abs((FIRST * SECOND) % 30)]
+	app.get("/api/v1/image", async (request, response) => {
+		const firstInitial = request.query.first as string | undefined
+		const secondInitial = request.query.second as string | undefined
 
-	console.log(image.urls.raw)
+		if (!firstInitial || !secondInitial)
+			return response.status(403).send("Bad Request: Please provide first and second initial by query")
+
+		if (firstInitial.length !== 1 || secondInitial.length !== 1)
+			return response.status(403).send("Bad Request: Initials can only be 1 character long.")
+
+		const validatedFirstInital = firstInitial.trim().toLowerCase()
+		const validatedSecondInital = secondInitial.trim().toLowerCase()
+
+		if (!ALPHABET.includes(validatedFirstInital) || !ALPHABET.includes(validatedSecondInital))
+			return response.status(403).send("Bad Request: Initials may only use characters from the latin alphabet (a-z)")
+
+		const imageIndex = ALPHABET.indexOf(validatedFirstInital) * ALPHABET.indexOf(validatedSecondInital)
+
+		// Requesting the image from unsplash
+		const collection = await fetchCollection("4760062", Math.floor((imageIndex) / 30))
+		const image = collection[Math.abs((imageIndex) % 30)]
+
+		return response.json({ raw: image.urls.raw, alt: image.alt_description, width: image.width, height: image.height })
+	})
+
+	app.listen(3000)
 })().then(_ => {
 	/* */
 })
